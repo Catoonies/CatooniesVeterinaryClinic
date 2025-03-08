399c296d9092fdca85d28122f22ce62279e551bf
Catoonies <cvcvet17@gmail.com>
2025-03-08 22:21:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("bookingForm").addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        let nama = document.getElementById("nama").value;
-        let telefon = document.getElementById("telefon").value;
-        let perkhidmatan = document.getElementById("perkhidmatan").value;
-        
-        let message = `Tempahan berjaya!\nNama: ${nama}\nTelefon: ${telefon}\nPerkhidmatan: ${perkhidmatan}`;
-        alert(message);
-        
-        this.reset();
-    });
-});
+function goToPage(page) {
+    window.location.href = `halaman${page}.html`;
+}
