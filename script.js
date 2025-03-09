c4c7399eec3d537bfd9665a07d69bb03d6f5ab3a
Catoonies <cvcvet17@gmail.com>
2025-03-09 13:22:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-/* script.js */
-document.getElementById("pemeriksaan").addEventListener("click", function() {
-    window.location.href = "halaman2.html";
-});
 
-document.getElementById("pertanyaan").addEventListener("click", function() {
-    window.location.href = "halaman12.html";
-});
