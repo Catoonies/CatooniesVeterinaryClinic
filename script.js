59534b2526060b00f482679227543624b5366fbf
Catoonies <cvcvet17@gmail.com>
2025-03-09 12:44:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-// Pastikan AOS diaktifkan untuk animasi scroll
-AOS.init({
-    duration: 800,  // Kelajuan animasi
-    easing: "ease-in-out",
-    once: true // Animasi hanya berlaku sekali
+/* script.js */
+document.getElementById("pemeriksaan").addEventListener("click", function() {
+    window.location.href = "halaman2.html";
 });
 
-// Navbar berubah warna apabila skrol
-window.addEventListener("scroll", function() {
-    let navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
-
-// Smooth scrolling apabila klik pada pautan dalam navbar
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute("href").substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop - 60,  // Laraskan offset
-                behavior: "smooth"
-            });
-        }
-    });
+document.getElementById("pertanyaan").addEventListener("click", function() {
+    window.location.href = "halaman12.html";
 });
